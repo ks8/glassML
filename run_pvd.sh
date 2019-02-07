19b0ffd76061020f8dd29cd1806f0f824940273f
Kirk Swanson <swansonkirk80@gmail.com>
2019-02-07 15:03:18 -0600

    Added multiple substrate temps option

--- run_pvd.sh
+++ run_pvd.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=2D-energy
-#SBATCH --output=output-pvd_2D-energy
-#SBATCH --error=errors-pvd_2D-energy
+#SBATCH --job-name=pvd-energy
+#SBATCH --output=output-pvd-energy
+#SBATCH --error=errors-pvd-energy
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --account=pi-depablo
 #SBATCH --partition=depablo-gpu
 #SBATCH --gres=gpu:1
+#SBATCH --time=48:00:00
 PATH=$PATH:/home/swansonk1/DASH-7-9-2018/md_engine/
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/swansonk1/DASH-7-9-2018/md_engine/build
 module load cuda/8.0
 module load boost/1.62.0+openmpi-1.6+gcc-4.7
-python pvd_2D.py -output=2D-energy -deposition_runs=55 -substrate_temp=0.2345 -EIS_file=EIS -PE_file=PE 
+python pvd_2D.py --output=pvd-energy --substrate_temp 0.2345 0.214 0.1935 0.173 0.1525 0.132 0.1115 0.091 0.0705 0.05 --num_simulations=10
