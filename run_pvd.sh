f6c4c97b50d25b158f84997a846f8626b992526b
Kirk Swanson <swansonkirk80@gmail.com>
2019-02-27 22:26:22 -0600

    Updated with replicas option

--- run_pvd.sh
+++ run_pvd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=pvd-energy
-#SBATCH --output=output-pvd-energy
-#SBATCH --error=errors-pvd-energy
+#SBATCH --job-name=PVD-dataset-7
+#SBATCH --output=output-PVD-dataset-7
+#SBATCH --error=errors-PVD-dataset-7
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --account=pi-depablo
@@ -12,4 +12,4 @@ PATH=$PATH:/home/swansonk1/DASH-7-9-2018/md_engine/
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/swansonk1/DASH-7-9-2018/md_engine/build
 module load cuda/8.0
 module load boost/1.62.0+openmpi-1.6+gcc-4.7
-python pvd_2D.py --output=pvd-energy --substrate_temp 0.2345 0.214 0.1935 0.173 0.1525 0.132 0.1115 0.091 0.0705 0.05 --num_simulations=10
+python pvd_2D-Dan.py --output=PVD-dataset-7 --wall_spring_const=15 --num_turns_deposition 100000 --deposition_runs=120 --substrate_temp 0.18 --EIS_file=EIS_PVD-dataset-7 --PE_file=PE_PVD-dataset-7 --num_simulations=1 --num_replicas=50
