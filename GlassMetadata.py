53b24564e26c5559cd148557d2b73f8389b004f2
Kirk Swanson <swansonkirk80@gmail.com>
2019-03-31 23:42:37 -0400

    Made options for class labeling more general

--- GlassMetadata.py
+++ GlassMetadata.py
@@ -14,23 +14,22 @@ def process_metadata(args):
     data = []
     for root, subfolders, files in os.walk(args.data_dir):
         for f in files:
+
+            # Set path
             path = os.path.join(root, f)
-            category = re.findall(r'glass', path)
-            if len(category) == 1:
-                if args.pvd_dataset:
-                    material = 'LC'
-                else:
-                    material = 'glass'
-            else:
-                if args.pvd_dataset:
-                    material = 'PVD'
-                else:
-                    material = 'liquid'
-            if material == 'PVD':
+
+            # Set class label
+            if args.category_label_1 in f:
+                material = args.category_label_1
+            elif args.category_label_2 in f:
+                material = args.category_label_2
+
+            # Set uid
+            if args.software == 'DASH':
                 uid_start_index = f.find('--') + 2
                 uid_end_index = f.find('_normalized')
                 uid = f[uid_start_index:uid_end_index]
-            else:
+            elif args.software == 'LAMMPS':
                 uid_start_index = f.find('.') + 1
                 uid = f[uid_start_index:uid_start_index + 5]
             data.append({'path': path, 'label': material, 'uid': uid})
@@ -47,7 +46,12 @@ def main():
 
     parser.add_argument('--data_dir', type=str, dest='data_dir', default=None, help='Directory containing data')
     parser.add_argument('--out_dir', type=str, dest='out_dir', default=None, help='Directory name for metadata')
-    parser.add_argument('--pvd_dataset', action='store_true',  default=False, help='If dataset is PVD vs LC glass')
+    parser.add_argument('--category_label_1', type=str, dest='category_label_1', default=None,
+                        help='Name of one of two class labels')
+    parser.add_argument('--category_label_2', type=str, dest='category_label_2', default=None,
+                        help='Name of the other class label')
+    parser.add_argument('--software', type=str, dest='software', default=None,
+                        help='DASH or LAMMPS')
 
     args = parser.parse_args()
     process_metadata(args)
