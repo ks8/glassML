f645ea31ac49f51c8f3a77a2b296875ace55e512
Kirk Swanson <swansonkirk80@gmail.com>
2019-06-28 11:52:23 -0500

    Rearranging files

---
+++ CNN/create_logger.py
@@ -0,0 +1,39 @@
+import logging
+import os
+
+
+def create_logger(name: str, save_dir: str = None, quiet: bool = False) -> logging.Logger:
+    """
+    Creates a logger with a stream handler and two file handlers.
+    The stream handler prints to the screen depending on the value of `quiet`.
+    One file handler (verbose.log) saves all logs, the other (quiet.log) only saves important info.
+    :param name: The name of the logger.
+    :param save_dir: The directory in which to save the logs.
+    :param quiet: Whether the stream handler should be quiet (i.e. print only important info).
+    :return: The logger.
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.propagate = False
+
+    # Set logger depending on desired verbosity
+    ch = logging.StreamHandler()
+    if quiet:
+        ch.setLevel(logging.INFO)
+    else:
+        ch.setLevel(logging.DEBUG)
+    logger.addHandler(ch)
+
+    if save_dir is not None:
+        if save_dir != '':
+            os.makedirs(save_dir, exist_ok=True)
+
+        fh_v = logging.FileHandler(os.path.join(save_dir, 'verbose.log'))
+        fh_v.setLevel(logging.DEBUG)
+        fh_q = logging.FileHandler(os.path.join(save_dir, 'quiet.log'))
+        fh_q.setLevel(logging.INFO)
+
+        logger.addHandler(fh_v)
+        logger.addHandler(fh_q)
+
+    return logger
